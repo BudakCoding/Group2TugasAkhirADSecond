cc82e15189d9320142fa8db21044775a827e2538
TheRealBuluBebek <57930537+TheRealBuluBebek@users.noreply.github.com>
2020-05-06 18:06:55 +0700

    Add project files.

---
+++ MessageBox.vb
@@ -0,0 +1,8 @@
+﻿Public Class MessageBoxs
+    Private Sub YesCheck_Click(sender As Object, e As EventArgs) Handles YesCheck.Click
+        If ErrorText.Tag = "Admin" Then
+            AdminLogin.Show()
+            Me.Close()
+        End If
+    End Sub
+End Class
---
+++ Selection.vb
@@ -0,0 +1,18 @@
+﻿Public Class Selection
+    Private Sub OrderButton_Click(sender As Object, e As EventArgs) Handles OrderButton.Click
+
+    End Sub
+
+    Private Sub openformadmin()
+        AdminLogin.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub AdminButton_Click(sender As Object, e As EventArgs) Handles AdminButton.Click
+        openformadmin()
+    End Sub
+
+    Private Sub AdminLabel_Click(sender As Object, e As EventArgs) Handles AdminLabel.Click
+        openformadmin()
+    End Sub
+End Class
